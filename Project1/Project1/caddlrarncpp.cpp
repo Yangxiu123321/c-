d336c0248d761f00ff980a557bd7969e1a7d98ac
Neu <20164278@stu.neu.edu.cn>
2017-11-05 17:07:14 +0800

    蜂鸣器位置

--- Project1/Project1/caddlrarncpp.cpp
+++ Project1/Project1/caddlrarncpp.cpp
@@ -3,26 +3,10 @@
 #include <string>
 #include <cstring>
 #include <vector>
+using namespace std;
+using namespace cv;
 void main(int argc,char *argv[])
 {
-	//定义一个数组
-	//std::vector<std::vector<int>>data(10);
-	std::vector<std::vector<int>>data;
-	std::vector<int>line;
-	for (int i = 0; i < 10; i++) {
-		data.push_back(line);
-	}
-
-	for (int i = 0; i < 10; i++) {
-		for (int j = 0; j < 20; j++) {
-			data[i].push_back(j);
-		}
-	}
-	for (int i = 0; i < 10; i++) {
-		for (int j = 0; j < data[i].size(); j++) {
-			std::cout << data[i][j];
-		}
-		std::cout << std::endl;
-	}
+	printf("my name is yangxiu");
 	system("pause");
 }
