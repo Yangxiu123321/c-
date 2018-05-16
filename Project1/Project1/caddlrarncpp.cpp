a6df78ba027ee874c7a5244741990c22dd30a795
DESKTOP-5TNB2UL\23914 <20164278@stu.neu.edu.cn>
2018-05-16 22:41:56 +0800

    3.2.2string 对象的读写

--- Project1/Project1/caddlrarncpp.cpp
+++ Project1/Project1/caddlrarncpp.cpp
@@ -1,28 +1,15 @@
 #include <iostream>
+#include <string>
 //可以引用左操作数中的右操作数
 using std::cout;
 using std::endl;
+using std::cin;
+using std::string;
 
-typedef unsigned char uchar;
-
-typedef struct
+void main(void)
 {
-	int handleNear;
-	int handleFar;
-	int operator[](uchar i)
-	{
-		if (i == 0) { return handleNear; }
-		else if (i == 1) { return handleFar; }
-		else { cout << "Camera handle access denied!" << endl; return -1; }
-	}
-}debug;
-
-void main()
-{
-	debug exercise;
-	exercise.handleFar = 10;
-	exercise.handleNear = 12;
-	cout << exercise[0] << endl;
-	cout << exercise[1] << endl;
+	string s;
+	cin >> s;
+	cout << s <<endl;
 }
 
