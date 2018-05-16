99cbe984442c1017b032d27fcdce73f95bcc1dc5
DESKTOP-5TNB2UL\23914 <20164278@stu.neu.edu.cn>
2018-05-16 23:06:22 +0800

    string对象的相关操作

--- Project1/Project1/caddlrarncpp.cpp
+++ Project1/Project1/caddlrarncpp.cpp
@@ -8,8 +8,24 @@ using std::string;
 
 void main(void)
 {
-	string s;
-	cin >> s;
-	cout << s <<endl;
+	string s("yang is good\r\n");
+	//size empty
+	cout << s.size() << s.empty() << endl;
+	
+	//s.size()返回的时string::size_type类型可以是usigned的很多类型，保证足够大
+	
+	//对象赋值
+	string ss;
+	ss = s;
+	cout << ss << endl;
+
+	//对象相加
+	string add;
+	add = s + ss;
+	cout << add << endl;
+
+	//下标索引
+	cout << add[0] << endl;
+
 }
 
