db917364fa24cc2cf6f758fcc6cf995dda2c03d0
DESKTOP-5TNB2UL\23914 <20164278@stu.neu.edu.cn>
2018-05-17 13:28:02 +0800

    bitset对象及操作

--- Project1/Project1/caddlrarncpp.cpp
+++ Project1/Project1/caddlrarncpp.cpp
@@ -1,28 +1,21 @@
 #include <iostream>
-#include <vector>
-using namespace std;
+#include <bitset>
+using std::bitset;
+using std::cout;
+using std::endl;
+using std::string;
 void main(void)
 {
-	//创建vector，预分配内存
-	vector<int> inve1(10,-1);//10个元素
-	cout << inve1[0] << endl;
+	//usigned初始化
+	bitset<16> b1(0xffff);
+	cout << b1[0] << endl;
 
-	//复制
-	vector<int> inve2(inve1);
-	cout << inve2[9] << endl;
-
-	//在末尾添加一个数
-	inve1.push_back(19);
-	cout << inve1[10] << endl;
-	//inve1[11] = 20;错误，必须要已知元素才能这样操作
-	
-	//迭代器访问元素
-	vector<int>::iterator iter1 = inve1.begin();
-	cout << iter1[0] <<" "<< *iter1 << endl;
-	for (vector<int>::iterator iter2 = inve2.begin(); iter2 != inve2.end(); ++iter2)
+	//string初始化
+	string s1("1100");
+	bitset<32> b2(s1);
+	for (char i = 0; i < 4; i++)
 	{
-		*iter2 = 0;
+		cout << b2[i] << endl;
 	}
-
 }
 
