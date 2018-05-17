70a7d48bfd54594aae3ac1ada3ae922fb532574f
DESKTOP-5TNB2UL\23914 <20164278@stu.neu.edu.cn>
2018-05-17 12:45:53 +0800

    vector及对象操作

--- Project1/Project1/caddlrarncpp.cpp
+++ Project1/Project1/caddlrarncpp.cpp
@@ -1,31 +1,20 @@
 #include <iostream>
-#include <string>
-//可以引用左操作数中的右操作数
-using std::cout;
-using std::endl;
-using std::cin;
-using std::string;
-
+#include <vector>
+using namespace std;
 void main(void)
 {
-	string s("yang is good\r\n");
-	//size empty
-	cout << s.size() << s.empty() << endl;
+	//创建vector，预分配内存
+	vector<int> inve1(10,-1);//10个元素
+	cout << inve1[0] << endl;
+
+	//复制
+	vector<int> inve2(inve1);
+	cout << inve2[9] << endl;
+
+	//在末尾添加一个数
+	inve1.push_back(19);
+	cout << inve1[10] << endl;
+	//inve1[11] = 20;错误，必须要已知元素才能这样操作
 	
-	//s.size()返回的时string::size_type类型可以是usigned的很多类型，保证足够大
-	
-	//对象赋值
-	string ss;
-	ss = s;
-	cout << ss << endl;
-
-	//对象相加
-	string add;
-	add = s + ss;
-	cout << add << endl;
-
-	//下标索引
-	cout << add[0] << endl;
-
 }
 
