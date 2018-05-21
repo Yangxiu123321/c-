17dd3254959c0ac7514a8bc59c81653fa3834c9a
DESKTOP-5TNB2UL\23914 <20164278@stu.neu.edu.cn>
2018-05-21 14:31:22 +0800

    指向const对象的指针/用指针输出数组元素

--- Project1/Project1/caddlrarncpp.cpp
+++ Project1/Project1/caddlrarncpp.cpp
@@ -6,16 +6,42 @@ using std::endl;
 using std::string;
 void main(void)
 {
-	//usigned初始化
-	bitset<16> b1(0xffff);
-	cout << b1[0] << endl;
+	//使用指针访问数组元素
+	int ia[] = { 0,2,4,6,8 };
+	int *ip = ia;
+	cout << *ip << endl;
 
-	//string初始化
-	string s1("1100");
-	bitset<32> b2(s1);
-	for (char i = 0; i < 4; i++)
+	int *ip2 = ip + 4;
+	cout << *ip2 << endl;
+
+	ptrdiff_t n = ip2 - ip;
+	cout << "指针距离:" << n << endl;
+
+	//对空指针做减法
+	int *ip3 = NULL;
+	int *ip4 = NULL;
+	ptrdiff_t m = ip3 - ip4;
+	cout <<"空指针相减:"<< m << endl;
+
+	ip = &ia[4];
+	cout << *ip << endl;
+	
+	//用指针输出数组元素
+	const size_t arr_sz = 5;
+	for (int *pbegin = ia, *pend = ia + arr_sz; pbegin != pend; ++pbegin)
 	{
-		cout << b2[i] << endl;
+		cout << *pbegin << ' ';
+		cout << endl;
 	}
+
+	//指向const对象的指针
+	cout << "指向const对象的指针:" << endl;
+	const double pi = 3.14;
+	const double *cptr = &pi;
+	cout << *cptr << endl;
+
+	//创建动态数组
+	printf("创建动态数组:\r\n");
+
 }
 
