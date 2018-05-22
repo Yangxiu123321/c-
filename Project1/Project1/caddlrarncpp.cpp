93ec2c7d49f99098e7ca8f6579fa7e0e3881e43c
DESKTOP-5TNB2UL\23914 <20164278@stu.neu.edu.cn>
2018-05-22 12:47:51 +0800

    函数形参模板

--- Project1/Project1/caddlrarncpp.cpp
+++ Project1/Project1/caddlrarncpp.cpp
@@ -4,44 +4,23 @@ using std::bitset;
 using std::cout;
 using std::endl;
 using std::string;
+
+template <typename T>
+int compare(const T &v1, const T &v2)
+{
+	if (v1 < v2)return 1;
+	if (v2 < v1)return -1;
+	return 0;
+}
+
+
 void main(void)
 {
-	//使用指针访问数组元素
-	int ia[] = { 0,2,4,6,8 };
-	int *ip = ia;
-	cout << *ip << endl;
-
-	int *ip2 = ip + 4;
-	cout << *ip2 << endl;
-
-	ptrdiff_t n = ip2 - ip;
-	cout << "指针距离:" << n << endl;
-
-	//对空指针做减法
-	int *ip3 = NULL;
-	int *ip4 = NULL;
-	ptrdiff_t m = ip3 - ip4;
-	cout <<"空指针相减:"<< m << endl;
-
-	ip = &ia[4];
-	cout << *ip << endl;
-	
-	//用指针输出数组元素
-	const size_t arr_sz = 5;
-	for (int *pbegin = ia, *pend = ia + arr_sz; pbegin != pend; ++pbegin)
-	{
-		cout << *pbegin << ' ';
-		cout << endl;
-	}
-
-	//指向const对象的指针
-	cout << "指向const对象的指针:" << endl;
-	const double pi = 3.14;
-	const double *cptr = &pi;
-	cout << *cptr << endl;
-
-	//创建动态数组
-	printf("创建动态数组:\r\n");
-
+	int a = 0;
+	int b = 0;
+	int result = 10;
+	a = 16; b = 12;
+	result = compare(a, b);
+	cout << result << endl;
 }
 
