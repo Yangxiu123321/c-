fa64112aef8606422f1c0fecc435ef9f6c74cf4f
DESKTOP-5TNB2UL\23914 <20164278@stu.neu.edu.cn>
2018-05-25 19:36:17 +0800

    简单提交

--- Project1/Project1/caddlrarncpp.cpp
+++ Project1/Project1/caddlrarncpp.cpp
@@ -1,12 +1,36 @@
-#include <iostream>
 #include <thread>
 
-void print_hello()
+void do_something(int& i)
 {
-	printf("hello world\r\n");
+    ++i;
+	printf("%d\r\n",i);
 }
-void main(int argc,char **argv)
+
+struct func
 {
-	std::thread t(print_hello);
-	t.join();
+    int& i;
+
+    func(int& i_):i(i_){}
+
+    void operator()()
+    {
+        for(unsigned j=0;j<1000000;++j)
+        {
+            do_something(i);
+        }
+    }
+};
+
+
+void oops()
+{
+    int some_local_state=0;
+    func my_func(some_local_state);
+    std::thread my_thread(my_func);
+	my_thread.join();
+}
+
+int main()
+{
+    oops();
 }
