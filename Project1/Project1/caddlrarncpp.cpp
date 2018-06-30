ae35c8b6808c6125a97317b90b1fe7d5096d48eb
DESKTOP-5TNB2UL\23914 <20164278@stu.neu.edu.cn>
2018-06-30 10:06:36 +0800

    Sales_item书店

--- Project1/Project1/caddlrarncpp.cpp
+++ Project1/Project1/caddlrarncpp.cpp
@@ -1,15 +1,29 @@
-#include "stdio.h"
 #include <iostream>
-#include<string.h>
 #include "Sales_item.h"
-using namespace std;
-int main(void)
+int main()
 {
-	// 多次刷新可能会导致程序执行缓慢
-	int num;
-	cin >> num;
-	// flush为刷新输出缓存区
-	cout << num << '\n' << flush;
-	cerr << "no" << endl;
-	clog << "log" << endl;
+	Sales_item total;
+	if (std::cin >> total)
+	{
+		Sales_item trans;
+		while (std::cin >> trans)
+		{
+			if (total.isbn() == trans.isbn())
+			{
+				total += trans;
+			}
+			else
+			{
+				std::cout << total << std::endl;
+				total = trans;
+			}
+			std::cout << total << std::endl;
+		}
+	}
+	else
+	{
+		std::cerr << "No data?!" << std::endl;
+		return -1;
+	}
+	return 0;
 }
