1a8db314515d26903c7de4325060a2641a52b10e
DESKTOP-5TNB2UL\23914 <20164278@stu.neu.edu.cn>
2018-07-28 18:57:07 +0800

    erase删除map中的元素

--- Project1/Project1/caddlrarncpp.cpp
+++ Project1/Project1/caddlrarncpp.cpp
@@ -18,9 +18,15 @@ int main(void)
 	mapStudent.insert(std::pair<int, std::string>(2, "std_two"));
 	mapStudent.insert(std::pair<int, std::string>(3, "std_three"));
 
-    // count查找map中是否有key，返回1则有，返回0则没有。
-	int isKey = 0;
-	isKey = mapStudent.count(3);
-	std::cout << isKey << std::endl;
+    // erase移除map中的元素
+	// 用迭代器删除
+	map<int, std::string>::iterator iter;
+
+	iter = mapStudent.find(1);
+	mapStudent.erase(iter);
+	int n = mapStudent.erase(1);
+	// 整片删除
+	mapStudent.erase(mapStudent.begin(), mapStudent.end());
+	cout << mapStudent.size() << std::endl;
 	return 0;
 }
