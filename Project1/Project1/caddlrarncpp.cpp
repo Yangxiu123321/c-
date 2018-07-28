b0f5500cd816dbdcf79496f5737eaee9d6012cf1
DESKTOP-5TNB2UL\23914 <20164278@stu.neu.edu.cn>
2018-07-28 16:27:44 +0800

    map用insetr插入pair数据

--- Project1/Project1/caddlrarncpp.cpp
+++ Project1/Project1/caddlrarncpp.cpp
@@ -1,24 +1,20 @@
-#include "iostream"
-#include "memory"
+#include <map>
+#include <string>
+#include <iostream>
 
 int main(void)
 {
-	/// shared_ptr指针的使用
-	int a = 0;
-	std::shared_ptr<int> ptra = std::make_shared<int>(a);
-	// copy
-	std::shared_ptr<int> ptra2(ptra);
-	std::shared_ptr<int> ptra3(ptra);
-	std::cout <<"shared_ptr:" <<  ptra.use_count() << std::endl;
+	// 关联容器map<索引，索引的数据> 类名;
+	std::map<int,std::string> mapStudent;
+	mapStudent.insert(std::pair<int,std::string>(1, "student_one"));
+	mapStudent.insert(std::pair<int,std::string>(2, "student_two"));
+	mapStudent.insert(std::pair<int,std::string>(3, "student_three"));
 
-	/// unique_ptr
-	// 绑定动态对象
-	std::unique_ptr<int> uptr(new int(10));
-	// std::unique_ptr<int> uptr2 = uptr;不能赋值
-	// std::unique_ptr<int> uptr2(uptr);// 不能拷贝
-	std::unique_ptr<int> uptr2 = std::move(uptr);
-	uptr2.release(); // 释放所有权
-	
-
-	/// std::weak_ptr()
+	std::map<int, std::string>::iterator iter = mapStudent.begin();
+	std::map<int, std::string>::iterator itend = mapStudent.end();
+	for (; iter != itend; iter++)
+	{
+		std::cout << iter->first << ' ' << iter->second << std::endl;
+	} 
+	return 0;
 }
