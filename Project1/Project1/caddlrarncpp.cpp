492450cc244278237323a5051359393aac09e32e
DESKTOP-5TNB2UL\23914 <20164278@stu.neu.edu.cn>
2018-07-29 14:52:33 +0800

    容器初始化

--- Project1/Project1/caddlrarncpp.cpp
+++ Project1/Project1/caddlrarncpp.cpp
@@ -1,85 +1,19 @@
-//第二种：仿函数的应用，这个时候结构体中没有直接的小于号重载，程序说明
-
-#include <iostream>
-
-#include <map>
-
-#include <string>
+#include <vector>
 
 using namespace std;
 
-typedef struct tagStudentinfo
+int main(void)
 {
-	int      niD;
-	string   strName;
-}Studentinfo; //学生信息
+	// 初始化一个容器
+	vector<int> stu_one;
+	// 初始一个大小为10的向量
+	vector<int> stu_two(10);
+	// 初始一个大小为10的向量且初始值为1的向量
+	vector<int> stu_three(10, 1);
+    // 用stu_one初始化stu_four
+	vector<int> stu_four(stu_one);
+	// 将a向量中从第0个到第2个(共3个)作为向量b的初始值
+	vector<int> stu_five(stu_one.begin(), stu_one.begin() + 2);
 
-
-
-class sort
-{
-public:
-	bool operator() (Studentinfo const &_A, Studentinfo const &_B) const
-	{
-		if (_A.niD < _B.niD)
-			return true;
-		if (_A.niD == _B.niD)
-			return _A.strName.compare(_B.strName) < 0;
-		return false;
-	}
-
-};
-
-
-
-int main()
-
-
-
-{   //用学生信息映射分数
-
-
-
-	map<Studentinfo, int, sort>mapStudent;
-
-
-
-	map<Studentinfo, int>::iterator iter;
-
-
-
-	Studentinfo studentinfo;
-
-
-
-	studentinfo.niD = 1;
-
-
-
-	studentinfo.strName = "student_one";
-
-
-
-	mapStudent.insert(pair<Studentinfo, int>(studentinfo, 90));
-
-
-
-	studentinfo.niD = 2;
-
-
-
-	studentinfo.strName = "student_two";
-
-
-
-	mapStudent.insert(pair<Studentinfo, int>(studentinfo, 80));
-
-
-
-	for (iter = mapStudent.begin(); iter != mapStudent.end(); iter++)
-
-
-
-		cout << iter->first.niD << ' ' << iter->first.strName << ' ' << iter->second << endl;
-
+	return 0;
 }
