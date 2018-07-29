a2cc6777155347a85559846b3ba4b75efeba1433
DESKTOP-5TNB2UL\23914 <20164278@stu.neu.edu.cn>
2018-07-29 22:19:00 +0800

    两个容器之间的赋值

--- Project1/Project1/caddlrarncpp.cpp
+++ Project1/Project1/caddlrarncpp.cpp
@@ -6,12 +6,25 @@ using namespace std;
 int main(void)
 {
 	// 初始化一个容器
+	// 可以在声明的时候初始化，具体见前面的Vector的定义/或者用for循环来进行赋值
 	vector<int> stu_one;
 
-	stu_one.insert(stu_one.begin(), 100);
-	// 只有第一个有元素了才能在第二个中插入
-	stu_one.insert(stu_one.begin() + 1, 10);
-	cout << stu_one[1] << endl;
+	vector<int> stu_two;
+
+	vector<int> stu_three;
+
+	stu_two.push_back(1);
+	stu_two.push_back(2);
+
+	std::cout << stu_two[1] << std::endl;
+
+	swap(stu_one, stu_two);
+
+	std::cout << stu_one[1] << std::endl;
+
+	stu_three.assign(stu_one.begin(), stu_one.end());
+
+	std::cout << stu_three[1] << std::endl;
 
 	return 0;
 }
