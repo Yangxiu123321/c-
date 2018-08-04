76438225685cfbc9ee5d3f8eb80250749fd9ab91
DESKTOP-5TNB2UL\23914 <20164278@stu.neu.edu.cn>
2018-08-04 14:29:53 +0800

    10个线程报数

--- Project1/Project1/caddlrarncpp.cpp
+++ Project1/Project1/caddlrarncpp.cpp
@@ -1,53 +1,49 @@
 
-//子线程报数
-
 #include <stdio.h>
 
 #include <process.h>
 
 #include <windows.h>
 
-int g_nCount;
+volatile long g_nLoginCount; //登录次数
 
-//子线程函数
+unsigned int __stdcall ThreadFun(void *pPM); //线程函数
 
-unsigned int __stdcall ThreadFun(PVOID pM)
+const int THREAD_NUM = 10; //启动线程数
+
+unsigned int __stdcall ThreadFun(void *pPM)
 
 {
 
-	g_nCount++;
+	Sleep(100); //some work should to do
 
-	printf("线程ID号为%4d的子线程报数%d\n", GetCurrentThreadId(), g_nCount);
+	g_nLoginCount++;
+
+	Sleep(50);
 
 	return 0;
 
 }
 
-//主函数，所谓主函数其实就是主线程执行的函数。
-
 int main()
 
 {
 
-	printf("     子线程报数 \n");
-
-	printf(" -- by MoreWindows( http://blog.csdn.net/MoreWindows ) --\n\n");
+	g_nLoginCount = 0;
 
 
 
-	const int THREAD_NUM = 10;
-
-	HANDLE handle[THREAD_NUM];
-
-
-
-	g_nCount = 0;
+	HANDLE  handle[THREAD_NUM];
 
 	for (int i = 0; i < THREAD_NUM; i++)
 
 		handle[i] = (HANDLE)_beginthreadex(NULL, 0, ThreadFun, NULL, 0, NULL);
 
+
+
 	WaitForMultipleObjects(THREAD_NUM, handle, TRUE, INFINITE);
+
+	printf("有%d个用户登录后记录结果是%d\n", THREAD_NUM, g_nLoginCount);
 
 	return 0;
 
