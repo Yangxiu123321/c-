48c5768b818d5ffc06f74655e082249dbed6ca9c
DESKTOP-5TNB2UL\23914 <20164278@stu.neu.edu.cn>
2018-08-04 14:45:34 +0800

    InterlockedIncrement变量递增原子操作

--- Project1/Project1/caddlrarncpp.cpp
+++ Project1/Project1/caddlrarncpp.cpp
@@ -1,23 +1,21 @@
 
 #include <stdio.h>
 
-#include <process.h>
-
 #include <windows.h>
 
 volatile long g_nLoginCount; //登录次数
 
-unsigned int __stdcall ThreadFun(void *pPM); //线程函数
+unsigned int __stdcall Fun(void *pPM); //线程函数
 
-const int THREAD_NUM = 10; //启动线程数
+const DWORD THREAD_NUM = 50;//启动线程数
 
-unsigned int __stdcall ThreadFun(void *pPM)
+DWORD WINAPI ThreadFun(void *pPM)
 
 {
 
 	Sleep(100); //some work should to do
 
-	g_nLoginCount++;
+	InterlockedIncrement((LPLONG)&g_nLoginCount);
 
 	Sleep(50);
 
@@ -29,21 +27,35 @@ int main()
 
 {
 
-	g_nLoginCount = 0;
+	printf("     原子操作 Interlocked系列函数的使用\n");
+
+	printf(" -- by MoreWindows( http://blog.csdn.net/MoreWindows ) --\n\n");
 
 
 
-	HANDLE  handle[THREAD_NUM];
+	//重复20次以便观察多线程访问同一资源时导致的冲突
 
-	for (int i = 0; i < THREAD_NUM; i++)
+	int num = 20;
 
-		handle[i] = (HANDLE)_beginthreadex(NULL, 0, ThreadFun, NULL, 0, NULL);
+	while (num--)
 
+	{
 
+		g_nLoginCount = 0;
 
-	WaitForMultipleObjects(THREAD_NUM, handle, TRUE, INFINITE);
+		int i;
 
-	printf("有%d个用户登录后记录结果是%d\n", THREAD_NUM, g_nLoginCount);
+		HANDLE  handle[THREAD_NUM];
+
+		for (i = 0; i < THREAD_NUM; i++)
+
+			handle[i] = CreateThread(NULL, 0, ThreadFun, NULL, 0, NULL);
+
+		WaitForMultipleObjects(THREAD_NUM, handle, TRUE, INFINITE);
+
+		printf("有%d个用户登录后记录结果是%d\n", THREAD_NUM, g_nLoginCount);
+
+	}
 
 	return 0;
 
