95b8d87d4a2e24d518a408185e7fee66b580bc37
DESKTOP-5TNB2UL\23914 <20164278@stu.neu.edu.cn>
2018-08-06 11:29:54 +0800

    va_list、va_start、va_arg、va_end

--- Project1/Project1/caddlrarncpp.cpp
+++ Project1/Project1/caddlrarncpp.cpp
@@ -1,12 +1,35 @@
-#include <iostream>
-#include <vector>
-
-using namespace std;
-
-int main(void)
+#include <stdio.h>
+#include <stdarg.h>
+class A
 {
-#define INDEX 10
-	int i = 100;
-#undef index
-	std::cout << INDEX << std::endl;
+public:
+	int function1(int a, int b);
+	int function2(int a, ...);
+};
+int A::function1(int a, int b)
+{
+	return a + b;
+}
+
+int A::function2(int a, ...)
+{
+	va_list ap;
+	va_start(ap, a);
+	int i;
+	int result = 0;
+	for (i = 0; i < a; i++)
+	{
+		result += va_arg(ap, int);
+	}
+	va_end(ap);
+	return result;
+}
+int main()
+{
+	int tempA, tempB;
+	A a;
+	tempA = a.function1(1, 2);
+	tempB = a.function2(3, 1, 2, 3);
+	printf("%d:%d\r\n", tempA, tempB);
+	return 0;
 }
