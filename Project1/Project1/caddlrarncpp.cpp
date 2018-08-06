44c15b18ed32dacb3cb97fcf1896ca87c58628ce
DESKTOP-5TNB2UL\23914 <20164278@stu.neu.edu.cn>
2018-08-06 15:32:09 +0800

    秒杀多线程第十篇 生产者消费者问题

--- Project1/Project1/caddlrarncpp.cpp
+++ Project1/Project1/caddlrarncpp.cpp
@@ -1,100 +1,175 @@
 
+//1生产者 1消费者 1缓冲区
+
+//使用二个事件，一个表示缓冲区空，一个表示缓冲区满。
+
+//再使用一个关键段来控制缓冲区的访问
+
 #include <stdio.h>
 
 #include <process.h>
 
 #include <windows.h>
 
-long g_nNum;
+//设置控制台输出颜色
 
-unsigned int __stdcall Fun(void *pPM);
-
-const int THREAD_NUM = 10;
-
-//信号量与关键段
-
-HANDLE            g_hThreadParameter;
-
-CRITICAL_SECTION  g_csThreadCode;
-
-int main()
+BOOL SetConsoleColor(WORD wAttributes)
 
 {
 
-	printf("     经典线程同步 信号量Semaphore\n");
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 
-	printf(" -- by MoreWindows( http://blog.csdn.net/MoreWindows ) --\n\n");
+	if (hConsole == INVALID_HANDLE_VALUE)
 
+		return FALSE;
 
+	return SetConsoleTextAttribute(hConsole, wAttributes);
 
-	//初始化信号量和关键段
+}
 
-	g_hThreadParameter = CreateSemaphore(NULL, 0, 1, NULL);//当前0个资源，最大允许1个同时访问
+const int END_PRODUCE_NUMBER = 10;   //生产产品个数
 
-	InitializeCriticalSection(&g_csThreadCode);
+int g_Buffer;                        //缓冲区
 
+									 //事件与关键段
 
+CRITICAL_SECTION g_cs;
 
-	HANDLE  handle[THREAD_NUM];
+HANDLE g_hEventBufferEmpty, g_hEventBufferFull;
 
-	g_nNum = 0;
+//生产者线程函数
 
-	int i = 0;
+unsigned int __stdcall ProducerThreadFun(PVOID pM)
 
-	while (i < THREAD_NUM)
+{
+
+	for (int i = 1; i <= END_PRODUCE_NUMBER; i++)
 
 	{
 
-		handle[i] = (HANDLE)_beginthreadex(NULL, 0, Fun, &i, 0, NULL);
+		//等待缓冲区为空
 
-		WaitForSingleObject(g_hThreadParameter, INFINITE);//等待信号量>0
+		WaitForSingleObject(g_hEventBufferEmpty, INFINITE);
 
-		++i;
+
+
+		//互斥的访问缓冲区
+
+		EnterCriticalSection(&g_cs);
+
+		g_Buffer = i;
+
+		printf("生产者将数据%d放入缓冲区\n", i);
+
+		LeaveCriticalSection(&g_cs);
+
+
+
+		//通知缓冲区有新数据了
+
+		SetEvent(g_hEventBufferFull);
 
 	}
-
-	WaitForMultipleObjects(THREAD_NUM, handle, TRUE, INFINITE);
-
-
-
-	//销毁信号量和关键段
-
-	DeleteCriticalSection(&g_csThreadCode);
-
-	CloseHandle(g_hThreadParameter);
-
-	for (i = 0; i < THREAD_NUM; i++)
-
-		CloseHandle(handle[i]);
 
 	return 0;
 
 }
 
-// __stdcall从右往左入栈
-unsigned int __stdcall Fun(void *pPM)
+//消费者线程函数
+
+unsigned int __stdcall ConsumerThreadFun(PVOID pM)
 
 {
 
-	int nThreadNum = *(int *)pPM;
+	volatile bool flag = true;
 
-	ReleaseSemaphore(g_hThreadParameter, 1, NULL);//信号量++
+	while (flag)
+
+	{
+
+		//等待缓冲区中有数据
+
+		WaitForSingleObject(g_hEventBufferFull, INFINITE);
 
 
 
-	Sleep(50);//some work should to do
+		//互斥的访问缓冲区
+
+		EnterCriticalSection(&g_cs);
+
+		SetConsoleColor(FOREGROUND_GREEN);
+
+		printf("  消费者从缓冲区中取数据%d\n", g_Buffer);
+
+		SetConsoleColor(FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
+
+		if (g_Buffer == END_PRODUCE_NUMBER)
+
+			flag = false;
+
+		LeaveCriticalSection(&g_cs);
 
 
 
-	EnterCriticalSection(&g_csThreadCode);
+		//通知缓冲区已为空
 
-	++g_nNum;
+		SetEvent(g_hEventBufferEmpty);
 
-	Sleep(0);//some work should to do
 
-	printf("线程编号为%d  全局资源值为%d\n", nThreadNum, g_nNum);
 
-	LeaveCriticalSection(&g_csThreadCode);
+		Sleep(10); //some other work should to do
+
+	}
+
+	return 0;
+
+}
+
+int main(void)
+
+{
+
+	printf("  生产者消费者问题   1生产者 1消费者 1缓冲区\n");
+
+	printf(" -- by MoreWindows( http://blog.csdn.net/MoreWindows ) --\n\n");
+
+
+
+	InitializeCriticalSection(&g_cs);
+
+	//创建二个自动复位事件，一个表示缓冲区是否为空，另一个表示缓冲区是否已经处理
+
+	g_hEventBufferEmpty = CreateEvent(NULL, FALSE, TRUE, NULL);
+
+	g_hEventBufferFull = CreateEvent(NULL, FALSE, FALSE, NULL);
+
+
+
+	const int THREADNUM = 2;
+
+	HANDLE hThread[THREADNUM];
+
+
+
+	hThread[0] = (HANDLE)_beginthreadex(NULL, 0, ProducerThreadFun, NULL, 0, NULL);
+
+	hThread[1] = (HANDLE)_beginthreadex(NULL, 0, ConsumerThreadFun, NULL, 0, NULL);
+
+	WaitForMultipleObjects(THREADNUM, hThread, TRUE, INFINITE);
+
+	CloseHandle(hThread[0]);
+
+	CloseHandle(hThread[1]);
+
+
+
+	//销毁事件和关键段
+
+	CloseHandle(g_hEventBufferEmpty);
+
+	CloseHandle(g_hEventBufferFull);
+
+	DeleteCriticalSection(&g_cs);
 
 	return 0;
 
